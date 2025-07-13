6791da29c8d8f00267cdea25b4e93a8979167b9d
sourav <souravas007@gmail.com>
2025-07-13 19:58:55 +0530

    Optimize code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 import asyncio
 from rich.progress import track
 
@@ -11,8 +10,6 @@ async def main():
 async def countdown(seconds, name):
     for _ in track(range(seconds), description=f"[bold blue]{name}"):
         await asyncio.sleep(1)
-    sys.stdout.write("\r\033[K")  # \r = go to start, \033[K = clear to end of line
-    sys.stdout.flush()
 
 def to_seconds(minutes):
     return minutes * 60
@@ -24,11 +21,7 @@ def parse_arguments():
     parser.add_argument("pomodoro_duration", type=int, help="Duration")
     parser.add_argument("break_duration", type=int, help="Break")
     args = parser.parse_args()
-    pomodoro_duration = args.pomodoro_duration
-    break_duration = args.break_duration
-    pomodoro_duration = to_seconds(pomodoro_duration)
-    break_duration = to_seconds(break_duration)
-    return pomodoro_duration, break_duration
+    return to_seconds(args.pomodoro_duration), to_seconds(args.break_duration)
 
 if __name__ == "__main__":
     asyncio.run(main())
